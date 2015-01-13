8452aabbeffc9c27c4938e7a2a8b5c434783887b
Patricia Henriquez <patriciahenriques288@gmail.com>
2015-01-13 16:32:10 -0500

    Created blog with front page with just puts

---
+++ HomeworkOne.rb
@@ -0,0 +1,17 @@
+#Blog Class
+# - front_page
+# #Post Class
+# - title
+# - date 
+# - text 
+ 
+class Blog
+	def front_page 
+		puts "This is Patricia's Blog"
+	end 
+end 
+ 
+ blog = Blog.new 
+
+ blog.front_page
+
