ac1c745dbd00c0485413c8ad67450b0837edd5c7
Patricia Henriquez <patriciahenriques288@gmail.com>
2015-01-14 17:03:22 -0500

    blog Advanced

--- HomeworkOne.rb
+++ HomeworkOne.rb
@@ -26,12 +26,42 @@ class Blog
 		@posts << post 
 	end 
 
+	def sponsored_posts (post)
+		@posts <<post 
+		return "***********#{@title} ******************
+#{@text}
+----------------"
+	end 
+
+
 	def front_page
 		puts @posts
 	end
+
+
+
+
 end 
  
+class SpondoredPosts
 
+	def initialize (title, text)
+	@title = title
+	@text= text 
+	# @date= Date.new
+	# puts "#{@title}"
+	# puts "******************"
+	# puts "#{@text}"
+	# puts "---------------"
+	end 
+
+	def to_s
+return "***********#{@title} ******************
+#{@text}
+----------------"
+	end 
+end 
+ 
 
 class Post
 # attr_accessor:title :date :text 
@@ -53,22 +83,22 @@ return "#{@title}
 ----------------"
 	end 
 
-	def front_page_posts
+# 	def front_page_posts
 
 
-	end
+# 	end
 
 end 
 
-patricia_blog = Blog.new 
+patricia_blog = Blog.new
 
 post1=Post.new('Post title 1','Post1 Text')
-post2=Post.new('Post title 2','Post2 Text')
+spondored_post2=SpondoredPosts.new('Post title 2','Post2 Text')
 post3=Post.new('Post title 3','Post3 Text')
 
 patricia_blog.add_front_page_posts(post1)
-patricia_blog.add_front_page_posts(post2)
-patricia_blog.add_front_page_posts(post2)
+patricia_blog.sponsored_posts(spondored_post2)
+patricia_blog.add_front_page_posts(post3)
 
 
 puts patricia_blog.front_page
