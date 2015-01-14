57ae3d1476f4722574fa4b4d045881bea66a5bfb
Patricia Henriquez <patriciahenriques288@gmail.com>
2015-01-13 20:27:53 -0500

    got it to work thanks to Fernando

--- HomeworkOne.rb
+++ HomeworkOne.rb
@@ -22,14 +22,16 @@ class Blog
 		@posts = []
 	end 
 
-	def front_page 
-		@posts << front_page_posts
+	def add_front_page_posts (post)
+		@posts << post 
 	end 
+
+	def front_page
+		puts @posts
+	end
 end 
  
- patricia_blog = Blog.new 
 
- puts patricia_blog.front_page
 
 class Post
 # attr_accessor:title :date :text 
@@ -51,23 +53,36 @@ return "#{@title}
 ----------------"
 	end 
 
-	def list_of_posts
+	def front_page_posts
+
 
 	end
 
 end 
 
+patricia_blog = Blog.new 
+
 post1=Post.new('Post title 1','Post1 Text')
 post2=Post.new('Post title 2','Post2 Text')
 post3=Post.new('Post title 3','Post3 Text')
 
+patricia_blog.add_front_page_posts(post1)
+patricia_blog.add_front_page_posts(post2)
+patricia_blog.add_front_page_posts(post2)
 
-front_page_posts= []
-front_page_posts << post1
-front_page_posts << post2
-front_page_posts << post3
+
+puts patricia_blog.front_page
+
+# front_page_posts= []
+# front_page_posts << post1
+# front_page_posts << post2
+# front_page_posts << post3
 
 # puts front_page_posts
+
+
+ 
+
 
 
 
